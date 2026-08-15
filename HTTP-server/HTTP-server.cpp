3d1fbb5a32b04d68e9c455055a349200b39128bf
MarkoB <m.j.berg@hotmail.com>
2026-08-15 21:04:44 +0300

    HTTP-serverin toteutus kokonaisuudessaan main.cpp tiedostossa.

--- HTTP-server/HTTP-server.cpp
+++ HTTP-server/HTTP-server.cpp
@@ -1,12 +1,7 @@
 ﻿// HTTP-server.cpp : Defines the entry point for the application.
 //
-
+#include <iostream>
 #include "HTTP-server.h"
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
-}
