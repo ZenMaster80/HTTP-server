14f2417937e00cb6447affbd5bae77cc81de140b
MarkoB <m.j.berg@hotmail.com>
2026-08-15 17:56:12 +0300

    Add project files.

---
+++ HTTP-server/HTTP-server.cpp
@@ -0,0 +1,12 @@
+﻿// HTTP-server.cpp : Defines the entry point for the application.
+//
+
+#include "HTTP-server.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ HTTP-server/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <winsock2.h>
+#include <ws2_tcpip.h>
+#include <string>
+#include <vector>
+
+int main()
+{
+	std::cout << "Toimii" << std::endl
+}
